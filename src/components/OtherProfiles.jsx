2e00de59c1625877f82f29d1aa254cdb6c0dfaf6
Daniele-996 <danielesanza@gmail.com>
2025-06-03 17:46:23 +0200

    adds border and line

--- src/components/OtherProfiles.jsx
+++ src/components/OtherProfiles.jsx
@@ -5,7 +5,7 @@ const OtherProfiles = ({ profile, button, title }) => {
   return (
     <>
       <Container>
-        <Row className="d-flex justify-content-center mx-auto my-2">
+        <Row className="d-flex justify-content-center mx-auto mt-2">
           <Col xs={3}>
             <Image
               src={profile.image}
@@ -14,7 +14,7 @@ const OtherProfiles = ({ profile, button, title }) => {
               roundedCircle
             />
           </Col>
-          <Col xs={9}>
+          <Col xs={9} className="mb-3">
             <span>
               <a className="text-dark text-decoration-none fw-bold" href="#">
                 {profile.name}
@@ -30,8 +30,8 @@ const OtherProfiles = ({ profile, button, title }) => {
               <h5 className="d-inline"> + </h5>
               {button}
             </Button>
-            <hr></hr>
           </Col>
+          <hr></hr>
         </Row>
       </Container>
     </>
