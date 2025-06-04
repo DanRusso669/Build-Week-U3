9596b6e9cc65ed43bdb6edd9238e021f958893c5
Dan Russo <rdaniele669@gmail.com>
2025-06-04 12:57:06 +0200

    adds modal for add experiences

--- src/components/sectionArea/Esperienza.jsx
+++ src/components/sectionArea/Esperienza.jsx
@@ -1,14 +1,20 @@
-import { Col, Image, Row } from "react-bootstrap";
+import { Button, Col, Form, Image, Modal, Row } from "react-bootstrap";
 import ButtonShowAll from "../ButtonShowAll";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchExperiences } from "../../redux/actions";
 import { format, parseISO } from "date-fns";
+import { Pencil } from "react-bootstrap-icons";
 
 const Esperienza = () => {
   const experiences = useSelector(state => state.experiences.content);
 
   const dispatch = useDispatch();
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     dispatch(fetchExperiences());
@@ -22,7 +28,51 @@ const Esperienza = () => {
   return (
     <>
       <div className="rounded-top-3 mt-2 bg-white p-4 border border-bottom-0" id="activityArea">
-        <h5>Esperienza</h5>
+        <div className="d-flex justify-content-between align-items-center">
+          <h5>Esperienza</h5>
+          <Pencil onClick={handleShow} />
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Aggiungi un'Esperienza</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Group className="mb-3" controlId="ruolo">
+                  <Form.Label>Ruolo</Form.Label>
+                  <Form.Control type="text" placeholder="Web Developer..." autoFocus required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="azienda">
+                  <Form.Label>Azienda</Form.Label>
+                  <Form.Control type="text" placeholder="Epicode School..." autoFocus />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="area">
+                  <Form.Label>Città</Form.Label>
+                  <Form.Control type="date" autoFocus />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="dataInizio">
+                  <Form.Label>Data Inizio</Form.Label>
+                  <Form.Control type="date" autoFocus />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="dataFine">
+                  <Form.Label>Data Fine</Form.Label>
+                  <Form.Control type="date" autoFocus />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="description">
+                  <Form.Label>Descrizione</Form.Label>
+                  <Form.Control as="textarea" rows={3} placeholder="Che mansioni hai svolto..." />
+                </Form.Group>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="success" onClick={handleClose}>
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
         {experiences.map(exp => (
           <Row className="mt-3 border-bottom">
             <Col xs={3} sm={2} md={3} lg={2} xl={1}>
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,9 +2,8 @@ export const GET_EXPERS = "GET_EXPERS";
 
 export const getExpersAction = data => ({ type: GET_EXPERS, payload: data });
 
-const key =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODNlZmY5M2IxMGJmMDAwMTVjZjIyYmEiLCJpYXQiOjE3NDkwMjA3ODksImV4cCI6MTc1MDIzMDM4OX0.e6YtXrzZbqS3FiunbFVMoILNDdddgoEDMO06sAz2uNg";
-const id = "683eff93b10bf00015cf22ba";
+const key = import.meta.env.VITE_STRIVE_TOKEN;
+const id = import.meta.env.VITE_STRIVE_ID;
 
 export const fetchExperiences = () => {
   return async dispatch => {
