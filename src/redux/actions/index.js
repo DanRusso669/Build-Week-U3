d6de9306fe6eb61478de721101e18699ffa7d8dc
Alessio Panconi <apanconi94@gmail.com>
2025-06-05 12:05:10 +0200

    BelCommit

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -4,8 +4,7 @@ export const GET_PROFILE = "GET_PROFILE";
 export const getExpersAction = (data) => ({ type: GET_EXPERS, payload: data });
 export const getProfile = (data) => ({ type: GET_PROFILE, payload: data });
 
-const key =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODNlZmY5M2IxMGJmMDAwMTVjZjIyYmEiLCJpYXQiOjE3NDkwMjA3ODksImV4cCI6MTc1MDIzMDM4OX0.e6YtXrzZbqS3FiunbFVMoILNDdddgoEDMO06sAz2uNg";
+const key = import.meta.env.VITE_STRIVE_TOKEN;
 const id = import.meta.env.VITE_STRIVE_ID;
 
 export const fetchExperiences = () => {
